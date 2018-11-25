a8ca52011dd2b1448339e9cf256473652206c977
Kevin <35283725+kpeter51@users.noreply.github.com>
2018-11-24 22:50:50 -0800

    spellcheck working

--- src/dictreenary/Dictreenary.java
+++ src/dictreenary/Dictreenary.java
@@ -58,13 +58,13 @@ public class Dictreenary implements DictreenaryInterface {
         if (isEmpty()) {return false;}
 
         char[] normalized_chars = normalizeWord(query).toCharArray();
-
         TTNode current = root;
 
         for (int i = 0; i < normalized_chars.length; ) {
             if (current == null) { return false; }
 
             int diff = compareChars(normalized_chars[i], current.letter);
+
             if (diff == 0) {
                 i++;
                 if (i == normalized_chars.length) { break; }
@@ -79,7 +79,15 @@ public class Dictreenary implements DictreenaryInterface {
     }
 
     public String spellCheck(String query) {
-        throw new UnsupportedOperationException();
+        if (hasWord(query)) { return query; }
+
+        char[] normalized_chars = normalizeWord(query).toCharArray();
+
+        for (int i = 0; i < normalized_chars.length - 1; i++) {
+            String correction = swapAdjacent(normalized_chars, i);
+            if (hasWord(correction)) { return correction; }
+        }
+        return null;
     }
 
     public ArrayList<String> getSortedWords() {
@@ -113,6 +121,14 @@ public class Dictreenary implements DictreenaryInterface {
         TTNode NewNode = (index == newWords.length - 1) ? new TTNode(newWords[index], true) : new TTNode(newWords[index], false);
         NewNode.mid = appendWord(newWords, index + 1);
         return NewNode;
+    }
+
+    private String swapAdjacent(char[] word, int index) {
+        char[] clone = word.clone();
+        char temp = clone[index];
+        clone[index] = clone[index + 1];
+        clone[index + 1] = temp;
+        return String.valueOf(clone);
     }
 
     // TTNode Internal Storage
